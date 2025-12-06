2af9408f6e6f6e15ff56387ffacff32a38ea8394
ReginaLuoYuxuan <Regina_Luo@126.com>
2025-12-06 17:29:46 -0600

    Update RestorationBar.tsx

--- RestorationBar.tsx
+++ RestorationBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { GamePhase } from '../../types';
+import { GamePhase } from './types';
 
 interface RestorationBarProps {
   cleanedCount: number;
