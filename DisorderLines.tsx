00b1d20a1ca931f99b1c2ec7e5faee6ca88532ae
ReginaLuoYuxuan <Regina_Luo@126.com>
2025-12-06 17:35:15 -0600

    Update DisorderLines.tsx

--- DisorderLines.tsx
+++ DisorderLines.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { GamePhase } from '../../types';
+import { GamePhase } from './types';
 
 interface DisorderLinesProps {
   phase: GamePhase;
